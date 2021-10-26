a8210408266d02b89aecdd1eba4d22324cca1c80
josepvazquezp <jose.vazquez@iteso.mx>
2021-10-26 12:21:31 -0500

    v

---
+++ stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+
+typedef void* TYPE;
+
+typedef struct node Node;
+
+typedef struct stack Stack;
+
+Stack* newStack();
+void push(Stack *stack, void *value);
+void *pop(Stack *stack);
+void *peek(Stack *stack);
+void display(Stack *stack);
+
+void sortStack(Stack *s, int(*compare)(void *a, void *b));
+
+#endif
